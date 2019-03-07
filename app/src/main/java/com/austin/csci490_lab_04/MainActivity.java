616b8b53074ea0152aaf580ab6d8eba92a783ae4
Austin Purtell <purtellap@g.cofc.edu>
2019-03-07 08:06:40 -0500

    url connection lab

--- app/src/main/java/com/austin/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/austin/csci490_lab_04/MainActivity.java
@@ -2,27 +2,51 @@ package com.austin.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b = (Button) findViewById(R.id.b);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                WebView webview = new WebView(getApplicationContext());
-                webview.setWebViewClient(new WebViewClient());
-                setContentView(webview);
-                webview.loadUrl("http://www.purtellap.students.cofc.edu/pw/");
-            }
-        });
+        Log.i("String: ",returnString("https://www.google.com"));
+
     }
+
+    private String returnString(String s){
+
+        String string = "";
+
+        try {
+            URL url = new URL(s);
+            URLConnection connection = url.openConnection();
+
+            InputStreamReader inputStreamReader = new InputStreamReader((connection.getInputStream()));
+
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            if(bufferedReader.readLine() != null){
+                string += bufferedReader.readLine();
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return string;
+    }
+
 }
